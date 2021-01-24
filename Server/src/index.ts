c18a922243aa3a68501a3ee56d434055e518bb28
rene_mbp <harrymillermail@googlemail.com>
2021-01-24 20:13:27 +0100

    feat: add application insights

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,7 +4,20 @@ const WSServer = require("ws").Server;
 const app = require("./http-server");
 import WebSockerServer from "./WebSocketServer";
 
-const server = http.createServer();
+const appInsights = require("applicationinsights");
+appInsights.setup().start();
+const logClient = appInsights.defaultClient;
+
+console.log(`start Server with NODE_ENV: ${process.env.NODE_ENV}`);
+logClient.trackTrace({ message: "server started", env: process.env.NODE_ENV });
+
+const envProduction = process.env.NODE_ENV === "production";
+
+const server = http.createServer((req, res) => {
+  if (envProduction) {
+    logClient.trackNodeHttpRequest({ request: req, response: res });
+  }
+});
 server.on("request", app);
 
 // create web socket server on top of a regular http server
