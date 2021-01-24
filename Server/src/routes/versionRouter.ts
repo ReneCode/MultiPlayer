81ea81a792be9cf31667c3bc41e0100c70d23c7f
rene_mbp <harrymillermail@googlemail.com>
2021-01-24 21:44:38 +0100

    fix: take config.json on build

--- Server/src/routes/versionRouter.ts
+++ Server/src/routes/versionRouter.ts
@@ -1,7 +1,10 @@
 import express, { Response, Request } from "express";
 import * as httpStatus from "http-status-codes";
 
-const config = require("../config.json");
+// https://stackoverflow.com/questions/40471152/tell-typescript-to-compile-json-files
+import config from "../config.json";
+
+// const config = require("../config.json");
 
 // https://auth0.com/blog/use-typescript-to-create-a-secure-api-with-nodejs-and-express-creating-endpoints/
 export const versionRouter = express.Router();
